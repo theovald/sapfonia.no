d8a70bf8902573fdb147965e74daad372ac951ef
theovald <gjoegjoe@outlook.com>
2023-05-18 13:04:43 +0200

    Works but fugly

---
+++ script.js
@@ -0,0 +1,13 @@
+// Smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+        // If the link is in the nav, hide the nav on mobile
+        if (this.parentElement.parentElement.classList.contains('nav-links')) {
+            document.querySelector('.nav-links').classList.remove('show');
+        }
+    });
+});
